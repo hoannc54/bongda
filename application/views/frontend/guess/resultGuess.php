6681861bf3f3136968c660d3f3c77563227d5a21
DoNguyetAnh <nhocwaltcott244@gmail.com>
2016-12-16 16:54:46 +0700

    Phan du doan

---
+++ application/views/frontend/guess/resultGuess.php
@@ -0,0 +1,125 @@
+<?php $this->load->view('frontend/templates/header');?>
+    <section id="main-body-wrapper" class="container">
+        <div class="row" id="main-body">
+            <div id="content" class="site-content col-md-8" role="main">
+                <ul class="breadcrumb">
+                    <li>
+                        <a href="#" class="breadcrumb_home">Home</a>
+                    </li>
+                    <li class="active">
+                        <a href="">NFL</a>
+                        <span class="raquo">/</span> McNulty: Arsenal&#8217;s draw shows need to goal
+                    </li>
+                </ul>
+                <h2 style="color:#000000">Dự đoán kết quả tỷ số</h2>
+
+                </br>
+                <?php echo "Kết quả dự đoán: Trận thi đấu giữa 2 đội: ".$doi1." VA ".$doi2 ; ?>
+
+            <?php
+               foreach ($history1 as $h ) {
+                   $thang_san_nha = $h['count_Thg'];
+                    $thua_san_nha = $h['count_Thua'];
+                    $hoa_san_nha = $h['count_Hoa'];
+                }
+//                ?>
+                <?php
+                foreach ($history2 as $h ) {
+                    $thua_san_khach = $h['count_Thg'];
+                    $thang_san_khach = $h['count_Thua'];
+                    $hoa_san_khach = $h['count_Hoa'];
+                }
+//                ?>
+                <?php
+                foreach ($date as $d) {
+                    $date = $d['year'];
+                    list($ngaydl, $thangdl, $namdl) = explode("/",$date);
+                }
+//                ?>
+
+
+                <?php
+
+                foreach ($team1_recent as $t1) {
+
+                    $rteam1_fthg_home = $t1['home_fthg'];
+                    $rteam1_fthg_away = $t1['away_fthg'];
+                    $rteam1_ftag_home = $t1['home_ftag'];
+                    $rteam1_ftag_away = $t1['away_ftag'];
+                    $rteam1_fequal_home = $t1['home_equal'];
+                    $rteam1_fequal_away = $t1['away_equal'];
+
+                }
+
+                ?>
+                <?php
+
+                foreach ($team2_recent as $t1) {
+                    $rteam2_fthg_home = $t1['home_fthg'];
+                    $rteam2_fthg_away = $t1['away_fthg'];
+                    $rteam2_ftag_home = $t1['home_ftag'];
+                    $rteam2_ftag_away = $t1['away_ftag'];
+                    $rteam2_fequal_home = $t1['home_equal'];
+                    $rteam2_fequal_away = $t1['away_equal'];
+
+                }
+
+                ?>
+                <?php
+                foreach ($team1_ranking as $r1) {
+                    $pteam1 = $r1['points'];
+                }?>
+                <?php
+                foreach ($team2_ranking as $r1) {
+                    $pteam2 = $r1['points'];
+                }
+                ?>
+
+                <?php
+                // lich su thi dau
+                $tong = $thang_san_nha+$thang_san_khach+$thua_san_nha+$thua_san_khach+$hoa_san_nha+$hoa_san_khach;
+                $x1 =($thang_san_nha+$thang_san_khach)/($tong+1);
+                //phong do hien tai trong mua giai ve ty le thang san nha
+                $tong_mua_giai_home = $rteam1_fthg_home + $rteam1_fequal_home + $rteam1_ftag_home;
+                $x2 = $rteam1_fthg_home/($tong_mua_giai_home+1);
+                $x3 = $pteam1/($pteam1+$pteam2+1);
+                $kq = ($x1+$x2+$x3)/3;
+                //Vi tri tren bang xep hang
+                echo "<br>";
+                echo "<h3>Tỷ lệ thắng của  ". $doi1." la </h3>".$kq;
+                echo "<br>";
+                echo "<h3> Theo tính toán của hệ thống: </h3>";
+                echo "<br>";
+                echo "<h4>So về Lich su thi dau: Tỷ lệ thắng trên sân nhà của đội chủ nhà là:</h4>".$x1;
+                echo "<br>";
+                echo "<h4>So về Phong độ hiện tại: Tỷ lệ thắng trên sân nhà của đội chủ nhà là:</h4>".$x2;
+                echo "<br>";
+                echo "<h4>So về Vị trí trên bảng xếp hạng: Tỷ lệ thắng trên sân nhà của đội chủ nhà là:</h4>".$x3;
+                echo "<br>";
+
+
+                ?>
+                <br>
+                <footer>
+
+
+                </footer>
+                <nav class="navigation post-navigation" role="navigation">
+
+                </nav><!-- .navigation -->
+
+                <span class='st_sharethis_large' displayText='ShareThis'></span>
+                <span class='st_facebook_large' displayText='Facebook'></span>
+                <span class='st_twitter_large' displayText='Tweet'></span>
+                <span class='st_linkedin_large' displayText='LinkedIn'></span>
+                <span class='st_pinterest_large' displayText='Pinterest'></span>
+                <span class='st_email_large' displayText='Email'></span>
+                <span class='st_googleplus_large' displayText='Google +'></span>
+
+            </div>
+
+            <?php $this->load->view('frontend/templates/sidebar');?>
+
+        </div>
+    </section>
+<?php $this->load->view('frontend/templates/footer');?>
